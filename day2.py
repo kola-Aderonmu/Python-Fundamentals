119515eb24c98590a5973746f3ae695646044414
boom <54904593+kola-Aderonmu@users.noreply.github.com>
2022-11-17 23:50:24 +0100

    DAY 2
    Day 2

---
+++ day2.py
@@ -0,0 +1,82 @@
+#--------DAY 2-------
+
+#STRING 
+# A string is an array of characters.
+
+#STRING SLICING-----This is the use of a state bracket [start:end:step]
+#example:
+#    mystrng = "I love chicken!"
+#    print(mystrng[1:6]) ----it excludes the main terminating point.
+#>>>love
+
+#example 2
+
+#splitted_val = mystring.split()
+#print(splitted_val)
+
+#mystrng = "I love chicken!"
+#splitted_val = mystring.split()
+#print(splitted_val)
+
+#splitted_val = mystring.split()
+
+#string Mthods
+ # capitalize()---just the first letter
+ # upper---caps all 
+# casefold() --changes to small caps all
+# center()----
+# find()----search the string for a specific value, returns the the first occurances of 
+#  
+#
+#DATA TYPE COVERSION--TYPECASTING
+
+#Convertig one datatype into another data type in order to convert for an ops
+
+# int, float and str
+
+# you cant convert str t int
+
+
+#------READ ON ASCII IN PYTHON--------
+
+# integer = 34
+#string = int(integer)
+#5555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555555
+
+# integer = 33
+
+#print(integer *2) --normall arithematics
+
+#integer = 33
+
+#string = str(integer) ----"33"
+#print(string)---------"33"
+#print (integer *4) ---33333333
+
+
+
+#-----ASSIGNMENT------
+#Decimal points in python, using deciamal points
+
+
+#write a string that is iterable
+
+
+
+char = "I love cooking"
+cat = char[:6] #--print 'i love'
+print(cat)
+
+for i in cat:
+    print(i) 
+    
+print(len(cat))
+
+print(char + ' ' + cat) #--concantenate
+
+#WHY USE TAB AND SPACE
+#if youre using a Spaces , memory is allocated 4 times while if you use TAB, memeroy is just allocated once compared to spaces.
+
+#assignment
+#Get an essay and get 10 words from it. pick from any start position you wish.
+

ada47712a4d8205b0108f55a9f41cf17a98a70b2
boom <54904593+kola-Aderonmu@users.noreply.github.com>
2022-11-22 21:44:14 +0100

    Add files via upload
    DAY 3

--- day2.py
+++ day2.py
@@ -62,6 +62,34 @@
 #write a string that is iterable
 
 
+fruit = 'banana'
+x = '*'.join(fruit.split('a'))
+print(x)
+
+fruit = 'banana'
+print(fruit.find('na'))
+
+
+names = 'Adekola*Hashim*Kabirat*Idris*Micheal'
+x = names.split('*')
+print(x)
+friends = ", ".join(x)
+
+names = 'Adekola*Hashim*Kabirat*Idris*Micheal'
+friends = names.replace("*", ", ")
+print(friends)
+
+print(friends)
+
+
+me = "cricket"
+you = "football"
+them = "hockey"
+string = " ".join([me, you, them])
+print(string)
+
+
+
 
 char = "I love cooking"
 cat = char[:6] #--print 'i love'
@@ -80,3 +108,55 @@ print(char + ' ' + cat) #--concantenate
 #assignment
 #Get an essay and get 10 words from it. pick from any start position you wish.
 
+essay = "It is not the critic who counts, not the man who points out how the strong man stumbled, or where the doer of deeds could have done better. The credit belongs to the man who is actually in the arena, whose face is marred by dust and sweat and blood, who strives valiantly, who errs and comes short again and again, who knows the great enthusiasms, the great devotions, and spends himself in a worthy cause, who at best knows achievement and who at the worst if he fails at least fails while daring greatly so that his place shall never be with those cold and timid souls who know neither victory nor defeat."
+ 
+jug = essay.split("*")
+
+print(jug)
+print("CHECK THIS:\n")
+first = ' '.join(jug[:6])
+print(first)
+
+#Real answer
+print("\n")
+words = "It is not the critic who counts, not the man who points out how the strong man stumbled, or where the doer of deeds could have done better."
+son = words.split()[:6]
+print(son) 
+print('\n\n')
+
+sons = "   ".join(son)
+print(sons)
+print("\n This is the whole sentence!")
+print(words)
+
+#To get the alphabets by character
+words = "It is not the critic who counts, not the man who points out how the strong man stumbled, or where the doer of deeds could have done better."
+for i in words:
+    print(i)
+
+print("\n\n", words[0:9]) #printing as words
+
+
+
+  
+
+    
+
+
+    
+    
+
+
+
+'''
+print("PRINT Just 10 words from the essay \n\n")
+cup = jug
+print(cup)
+
+
+
+
+
+#print("THIS IS GOING TO PRINT JUST 10 WORDS FROM ESSAY" + " " + )
+
+'''
